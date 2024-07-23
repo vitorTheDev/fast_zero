55037f4a8f54196dcf0863c88d3b225ea452abed
vitorthedev <netd777@gmail.com>
2024-07-23 11:28:34 -0300

    schemas com pydantic

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,17 +1,12 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return """
-    <html>
-      <head>
-        <title> Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1> Olá Mundo </h1>
-      </body>
-    </html>"""
+    return {'message': 'Olá Mundo!'}
